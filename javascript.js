90c229fa7cc1b7c09d31789f6965c0b840b15af2
Hong Le <hongrosebara@gmail.com>
2023-03-23 20:54:09 +0700

    formspree

--- javascript.js
+++ javascript.js
@@ -12,31 +12,37 @@ const closeModal = () => {
   document.getElementById("modal").style.display = "none";
 };
 
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
+
 consentForm.addEventListener("submit", function (e) {
   e.preventDefault()
-  const consentFormData = new FormData(consentForm)
-  const fullName = consentFormData.get('fullName')
 
-  modalText.innerHTML = `
-  <div class="modal-inner-loading">
-      <img src="images/loading.svg" class="loading">
-      <p id="upload-text">
-        Sending ... 
-      </p>
-  </div>`
-
-  setTimeout( () => {
-    document.getElementById("upload-text").innerText = `Success ...`
-    finalMessage()
-  }, 1500)
-
-  const finalMessage = () => {
-    setTimeout( () => {
-      document.getElementById('modal-inner').innerHTML = `
-      <h2>Thank you!</h2>
-      <p>I am happy to receive your message and will get back to you shortly!
-      </p>
-  ` 
-  }, 1500)
-  }
+  handleSubmit()
 })
